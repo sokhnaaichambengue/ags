408b72815be691a7546b9eedb4e2e830b55d7707
ibousv <devilsog2.0@gmail.com>
2024-05-25 23:52:11 +0000

    update

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,6 +1,16 @@
 const mysql = require("mysql")
 const { Sequelize} = require("sequelize")
-const sequelize = new Sequelize("ags","root","")
+const sequelize = new Sequelize("sequelize-test","root","",{
+    host:"localhost",
+    dialect:"mysql"
+})
 
+ sequelize.authenticate()
+     .then(()=>{
+        console.log("Connnected !")
+    })
+     .catch ((err)=> {
+    console.log("Error while loading connection")
+})
 
 module.exports = {sequelize,Sequelize}
