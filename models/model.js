fc984f0386a3a163aad66eb7e22be93497033440
ibousv <devilsog2.0@gmail.com>
2024-05-29 03:34:43 +0000

    fix

--- models/model.js
+++ models/model.js
@@ -1,251 +1,284 @@
 /* Models script */
 
-const { sequelize,Model} = require("../config/databaseConfig")
-const {DataTypes} = require("sequelize");
-
-class Utilisateur extends Model{}
+const { UPDATE } = require("sequelize/lib/query-types");
+const { sequelize, Model } = require("../config/databaseConfig")
+const { DataTypes } = require("sequelize");
 
 /* Models */
-class Administrateur extends Utilisateur{}
+
+class Utilisateur extends Model { }
+
+class Administrateur extends Utilisateur { }
 Administrateur.init(
     {
-        nom:{
-            type:DataTypes.STRING,
+        nom: {
+            type: DataTypes.STRING,
         },
-        prenom:{
-            type:DataTypes.STRING,
+        prenom: {
+            type: DataTypes.STRING,
         },
-        login:{
-            type:DataTypes.STRING,
-            allowNull:false,
-            primaryKey:true,
-            unique:true
+        login: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            unique: true
         },
-        motpasse:{
-            type:DataTypes.STRING,
-            allowNull:false,
-            validate:{
-                 notNull: true,
-                 notEmpty:true,
-                 len: [8,12]
+        motpasse: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: true,
+                notEmpty: true,
+                len: [8, 12]
             }
         }
     },
     {
-        sequelize:sequelize,
-        modelName:'administrateur',
-        timestamps:false
+        sequelize: sequelize,
+        modelName: 'administrateur',
+        timestamps: false
     }
 )
 
-class Enseignant extends Utilisateur{}
+class Enseignant extends Utilisateur { }
 Enseignant.init(
     {
-        nom:{
-            type:DataTypes.STRING,
+        nom: {
+            type: DataTypes.STRING,
         },
-        prenom:{
-            type:DataTypes.STRING,
+        prenom: {
+            type: DataTypes.STRING,
         },
-        login:{
-            type:DataTypes.STRING,
-            allowNull:false,
-            primaryKey:true,
-            unique:true
+        login: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            unique: true
         },
-        motpasse:{
-            type:DataTypes.STRING,
-            allowNull:false,
-            validate:{
-                 notNull: true,
-                 notEmpty:true,
-                 len: [8,12]
+        motpasse: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: true,
+                notEmpty: true,
+                len: [8, 12]
             }
         },
-        module:{
-            type:DataTypes.STRING,
+        module: {
+            type: DataTypes.STRING,
         }
     },
     {
-        sequelize:sequelize,
-        modelName:'enseignant',
-        timestamps:false
+        sequelize: sequelize,
+        modelName: 'enseignant',
+        timestamps: false
     }
 )
 
-class Filieres extends Model{}
+class Filieres extends Model { }
 Filieres.init(
     {
-        id:{
-            type:DataTypes.INTEGER,
-            autoIncrement:true,
-            primaryKey:true
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
         },
-        nom:{
-            type:DataTypes.STRING,
-            allowNull:false
+        nom: {
+            type: DataTypes.STRING,
+            allowNull: false
         },
-        nbretudiant:{
-            type:DataTypes.INTEGER,
+        nbretudiant: {
+            type: DataTypes.INTEGER,
         }
     },
     {
-        sequelize:sequelize,
-        modelName:'filieres',
-        timestamps:false
+        sequelize: sequelize,
+        modelName: 'filieres',
+        timestamps: false
     }
 )
 
-class Semestre extends Model{}
+class Semestre extends Model { }
 Semestre.init(
     {
-         id:{
-            type:DataTypes.INTEGER,
-            autoIncrement:true,
-            primaryKey:true
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
         },
-        nom:{
-            type:DataTypes.STRING,
+        nom: {
+            type: DataTypes.STRING,
         }
     },
     {
-        sequelize:sequelize,
-        modelName:'semestre',
-        timestamps:false
+        sequelize: sequelize,
+        modelName: 'semestre',
+        timestamps: false
     }
 )
 
-class Etudiant extends Utilisateur{}
+class Etudiant extends Utilisateur { }
 Etudiant.init(
     {
-        nom:{
-            type:DataTypes.STRING,
+        nom: {
+            type: DataTypes.STRING,
 
         },
-        prenom:{
-            type:DataTypes.STRING,
+        prenom: {
+            type: DataTypes.STRING,
 
         },
-        login:{
-            type:DataTypes.STRING,
-            allowNull:false,
-            primaryKey:true,
-            unique:true,
-            validate:{
-                 notNull: true,
-                 notEmpty:true,
-                 len: [8,12]
+        login: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            unique: true,
+            validate: {
+                notNull: true,
+                notEmpty: true,
+                len: [8, 12]
             }
 
         },
-        motpasse:{
-            type:DataTypes.STRING,
-            allowNull:false
+        motpasse: {
+            type: DataTypes.STRING,
+            allowNull: false
 
         },
-        filiere:{
-            type:DataTypes.STRING,
+        filiere: {
+            type: DataTypes.STRING,
 
         },
-        semestre:{
-            type:DataTypes.STRING,
+        semestre: {
+            type: DataTypes.STRING,
 
         }
     },
     {
-        sequelize:sequelize,
-        modelName:'etudiant',
-        timestamps:false
+        sequelize: sequelize,
+        modelName: 'etudiant',
+        timestamps: false
     }
 )
 
-class Evaluation extends Model{}
+class Evaluation extends Model { }
 Evaluation.init(
     {
-        id:{
-            type:DataTypes.INTEGER,
-            autoIncrement:true,
-            primaryKey:true
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
         },
-        duree:{
-            type:DataTypes.INTEGER,
+        duree: {
+            type: DataTypes.INTEGER,
         },
-        cours:{
-            type:DataTypes.STRING,
+        cours: {
+            type: DataTypes.STRING,
         },
-        filiere:{
-            type:DataTypes.STRING,
+        filiere: {
+            type: DataTypes.STRING,
         }
     },
     {
-        sequelize:sequelize,
-        modelName:'evaluation',
-        timestamps:false
+        sequelize: sequelize,
+        modelName: 'evaluation',
+        timestamps: false
     }
 )
 
-class Notes extends Model{}
+class Notes extends Model { }
 Notes.init(
     {
-         id:{
-            type:DataTypes.INTEGER,
-            autoIncrement:true,
-            primaryKey:true
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
         },
-        valeur:{
-            type:DataTypes.INTEGER,
-            validate:{
-                 notEmpty:true,
-                 len: [0,20]
+        valeur: {
+            type: DataTypes.INTEGER,
+            validate: {
+                notEmpty: true,
+                len: [0, 20]
             }
         },
-        evaluation:{
-            type:DataTypes.INTEGER,
+        evaluation: {
+            type: DataTypes.INTEGER,
         },
-        etudiant:{
-            type:DataTypes.STRING,
-            allowNull:false
+        etudiant: {
+            type: DataTypes.STRING,
+            allowNull: false
         },
-        semestre:{
-            type:DataTypes.INTEGER,
+        semestre: {
+            type: DataTypes.INTEGER,
         }
     },
     {
-        sequelize:sequelize,
-        modelName:'notes',
-        timestamps:false
+        sequelize: sequelize,
+        modelName: 'notes',
+        timestamps: false
     }
 )
 
-class Cours extends Model{}
+class Cours extends Model { }
 Cours.init(
     {
-        nom:{
-            type:DataTypes.STRING,
-            primaryKey:true,
-            unique:true,
+        nom: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            unique: true,
 
         },
-        enseignant:{
-            type:DataTypes.STRING,
+        enseignant: {
+            type: DataTypes.STRING,
         },
-        lieu:{
-            type:DataTypes.STRING,
+        lieu: {
+            type: DataTypes.STRING,
         }
     },
     {
-        sequelize:sequelize,
-        modelName:'cours',
-        timestamps:false
+        sequelize: sequelize,
+        modelName: 'cours',
+        timestamps: false
     }
 )
 
-/* */
+/* Association */
+
+Filieres.hasMany(Etudiant)
+Etudiant.belongsTo(Filieres, { as: 'FilieresEtudiant', foreignKey: 'FK_fe', onDelete: 'CASCADE' })
+
+Filieres.belongsToMany(Evaluation, { as: 'FilieresEvaluation', through: 'assos_fe' })
+Evaluation.belongsToMany(Filieres, { as: 'FilieresEvaluation', through: 'assos_fe' })
+
+Evaluation.hasMany(Notes)
+Notes.belongsTo(Evaluation, { as: 'EvaluationNotes', foreignKey: 'FK_evaluation_notes', onDelete: 'CASCADE' })
+
+Evaluation.belongsToMany(Cours, { as: 'EvaluationCours', through: 'assos_ec' })
+Cours.belongsToMany(Evaluation, { as: 'EvaluationCours', through: 'assos_ec' })
+
+Cours.belongsToMany(Notes, { as: 'CoursNotes', through: 'assos_cn' })
+Notes.belongsToMany(Cours, { as: 'CoursNotes', through: 'assos_cn' })
+
+Cours.belongsToMany(Etudiant, { as: 'CoursEtudiant', through: 'assos_ce' })
+Etudiant.belongsToMany(Cours, { as: 'CoursEtudiant', through: 'assos_ce' })
+
+Enseignant.hasMany(Cours)
+Cours.belongsTo(Enseignant, { as: 'EnseignantCours', foreignKey: 'FK_enseignant_cours', onDelete: 'CASCADE' })
+
+Etudiant.hasMany(Notes)
+Notes.belongsTo(Etudiant, { as: 'EtudiantNotes', foreignKey: 'FK_etudiant_notes', onDelete: 'CASCADE' })
+
+Semestre.hasMany(Notes)
+Notes.belongsTo(Semestre, { as: 'SemestreEtudiant', foreignKey: 'FK_semestre_notes', onDelete: 'CASCADE' })
+
+Semestre.belongsToMany(Etudiant, {  as:'SemestreEtudiant',through: 'assos_se' })
+Etudiant.belongsToMany(Semestre, {  as:'SemestreEtudiant',through: 'assos_se' })
+
 
 module.exports =
-    {
-        Etudiant,Enseignant,
-        Administrateur,Cours,
-        Notes,Filieres,
-        Evaluation,Semestre
-    }
+{
+    Etudiant, Enseignant,
+    Administrateur, Cours,
+    Notes, Filieres,
+    Evaluation, Semestre
+}
