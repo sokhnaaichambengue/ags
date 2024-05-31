6c1338a1990cba877a44b9a5eb22f9387140ce04
ibousv <devilsog2.0@gmail.com>
2024-05-31 17:25:10 +0000

    update

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,36 +1,101 @@
 /* Admin Controller */
-const { Administrateur, sequelize } = require('../models/model')
+const { Administrateur, sequelize, Etudiant, Enseignant, Cours, Notes } = require('../models/model')
 //const {session} = require('./index')
 const admin = {
-    getHome: (req, res) =>{
+    getHome: (req, res) => {
         res.render('admin-view')
     },
 
     // Create function
     createEtudiant: (req, res) => {
+        const { nom, prenom, login, motpasse, filiere, semestre } = req.body;
+        const etudiant = {
+            nom,
+            prenom,
+            login,
+            motpasse,
+            filiere,
+            semestre
+        }
+
+        Etudiant.create(etudiant).then(() => {
+            res.send("L' étudiant a été créé avec succès")
+        }).catch(err => {
+            res.end("Une erreur s'est produite")
+        })
 
     },
+
     createEnseignant: (req, res) => {
-        
+        const { nom, prenom, login, motpasse, module } = req.body;
+        Enseignant.create({
+            nom,
+            prenom,
+            login,
+            motpasse,
+            module
+        })
+
+        const enseignant = {
+            nom,
+            prenom,
+            login,
+            motpasse,
+            module
+        }
+
+        Enseignant.create(enseignant).then(() => {
+            res.send("L'enseignant a été créé avec succès")
+        }).catch(err => {
+            res.end("Une erreur s'est produite")
+        })
     },
     createCours: (req, res) => {
+        const { nom, enseignant,lieu } = req.body;
+        const cours = {
+            nom,
+            enseignant,
+            lieu
+        }
 
+        Cours.create(cours).then(()=>{
+            res.send("Le cours a été créé avec succès")
+        }).catch(err => {
+            res.end("Une erreur s'est produite")
+        })
     },
     createNote: (req, res) => {
+        const { valeur,evaluation,etudiant,notes } = req.body;
+        const note = {
+            valeur,
+            evaluation,
+            etudiant,
+            cours,
+            notes
+        }
 
+        Notes.create(note).then(()=> {
+            res.send("La note a été créé avec succès")
+        }).catch(err => {
+            res.end("Une erreur s'est produite")
+        })
     },
 
-    // Insert function
-    insertEtudiant: (req, res) => {
+    // Read function
+    displayEtudiant: (req, res) => {
+        Etudiant.findAll().then((etudiants) => {
+            res.send({ etudiants })
+        }).catch(err => {
+            res.end("Une erreur s'est produite")
+        })  
+    },
+    displayEnseignant: (req, res) => {
 
     },
-    insertEnseignant: (req, res) => {
+    displayCours: (req, res) => {
 
     },
-    insertCours: (req, res) => {
-
-    },
-    insertNote: (req, res) => {
+    displayNote: (req, res) => {
 
     },
 
@@ -69,5 +134,5 @@ const admin = {
     }
 
 }
-module.exports = admin
 
+module.exports = admin
